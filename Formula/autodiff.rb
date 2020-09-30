f161dece057ec1b5d77e25fe5144e66cceea15e0
Caleb Xu <calebcenter@live.com>
2020-09-30 15:13:37 -0400

    autodiff 0.5.11

--- Formula/autodiff.rb
+++ Formula/autodiff.rb
@@ -1,8 +1,8 @@
 class Autodiff < Formula
   desc "Automatic differentiation made easier for C++"
   homepage "https://autodiff.github.io"
-  url "https://github.com/autodiff/autodiff/archive/v0.5.10.tar.gz"
-  sha256 "d0e62994b7984014b2944d13f6ce9a75fc6b681d2d81f9051ec44410912dc5d7"
+  url "https://github.com/autodiff/autodiff/archive/v0.5.11.tar.gz"
+  sha256 "d6c5a7ea5459c98bfd2b6fd34a883b78ef1372e3e35ae07cd40fdf7c4a5c7576"
   license "MIT"
   head "https://github.com/autodiff/autodiff.git"
 
